4d3dec13f1c1d75aca8dfcd6fccf2cd6ef3db314
kizuto2006 <vhung7986@gmail.com>
2025-12-07 18:11:37 +0700

    add_plan

---
+++ Assets/Scripts/Plane/ButtonManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public List<ButtonPlane> buttons = new List<ButtonPlane>();
+    
+
+    private void Start()
+    {
+        foreach (var btn in buttons)
+            btn.Init(this);
+    }
+
+    public void NotifyButtonStateChanged()
+    {
+        foreach (var btn in buttons)
+        {
+            if (!btn.isPressed)
+            {
+                // có 1 nút chưa kích hoạt → toàn bộ chưa sẵn sàng
+                //OnAnyButtonReleased?.Invoke();
+                return;
+            }
+        }
+
+        // tất cả nút đều được đè lên
+        //OnAllButtonsActivated?.Invoke();
+        Debug.Log("plane");
+    }
+}
---
+++ Assets/Scripts/Plane/PlaneController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public GameObject bombPrefab;
+    public GameObject dropPoint;
+
+    public float dropDelay = 1.5f;    
+    private bool isActive = false;
+
+    private void Start()
+    {
+        GetComponent<PlaneController>().ActivatePlane();
+    }
+    private void Update()
+    {
+        if (!isActive) return;
+
+        // Máy bay bay sang phải
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+    }
+
+    public void ActivatePlane()
+    {
+        isActive = true;
+        Invoke(nameof(DropBomb), dropDelay);
+    }
+
+    void DropBomb()
+    {
+        Instantiate(bombPrefab, dropPoint.transform.position, Quaternion.identity);
+
+        // Nếu có animation thả bom, kích hoạt
+        GetComponent<Animator>().SetTrigger("Drop");
+    }
+
+    private void OnBecameInvisible()
+    {
+        // Khi ra khỏi màn hình thì tự xóa
+        Destroy(gameObject, 1f);
+    }
+}
