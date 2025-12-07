f9e78536736f56c6710bbb416cbe3f39f48cfcae
Duc_0102 <140878313+tranduc0102@users.noreply.github.com>
2025-12-07 11:10:26 +0700

    anim fall slide wall
    anim fall slide wall

---
+++ Assets/Scripts/Core/Recoder/PlayerInputData.cs
@@ -0,0 +1,9 @@
+[System.Serializable]
+public struct PlayerInputData
+{
+    public float horizontal;
+    public float vertical;
+    public bool IsRun;
+    public bool jumpPressed;
+    public bool dashPressed;
+}
